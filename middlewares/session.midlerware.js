e94eaa513c13a47a6cf3bee28a849ddf573ac9e0
Duc Minh <minh98it@gmail>
2020-03-19 09:33:14 +0700

    fix

--- middlewares/session.midlerware.js
+++ middlewares/session.midlerware.js
@@ -1,15 +1,15 @@
-var db = require('../db')
+
 const shortid = require('shortid');
 
-module.exports = function(req, res, next){ 
+module.exports = async function(req, res, next){ 
         if(!req.signedCookies.session_id){
             var session_id = shortid.generate();
             res.cookie('session_id',session_id,{
                 signed: true
             });
-            db.get('Session').push({
-                id : session_id,       
-            }).write();
+            // db.get('Session').push({
+            //     id : session_id,       
+            // }).write();
             
        };
         next();
